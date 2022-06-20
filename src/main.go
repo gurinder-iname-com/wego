e7a4aa40042995fffb7278c0facdf92cc6649935
Gurinder <gurinder@iname.com>
2022-06-20 20:18:42 +0100

    learning basics

--- src/main.go
+++ src/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+)
 
-func main(){
-	fmt.Println("Hello")
+func main() {
+	log.SetPrefix("WeGo: ")
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	IntUnderScoreTest()
+	FloatTest()
+	// for i := 0; i < 10; i++ {
+	// 	log.Println(i)
+	// }
+}
+
+func IntUnderScoreTest() {
+	var firstInt int = 1234567890
+	var secondInt = 1_234_567_890
+	log.Println(firstInt == secondInt)
+}
+
+func FloatTest() {
+	var firstFloat float32 = 1.2
+	log.Println(firstFloat)
 }
